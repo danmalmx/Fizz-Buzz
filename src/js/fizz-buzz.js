32ce8e222a4801868e91e98f89e4569253aa467d
danmalmx <danmalmx@gmail.com>
2018-09-10 08:58:41 +0200

    changes to be abke to make PR

--- src/js/fizz-buzz.js
+++ src/js/fizz-buzz.js
@@ -2,13 +2,15 @@ function FizzBuzz() {
     this.check = (number) => {
 
         if (number <= 0) {
-            return 'Positive integers only';   
+            return 'Positive int only';   
         } else if (number % 15 === 0) {
             return 'FizzBuzz';
         } else if (number % 5 === 0) {
             return 'Buzz';
         } else if (number % 3 === 0) {
             return 'Fizz';
+        } else if ((isNaN(number))) {
+            return 'That is not a number!'
         } else {
             return number;
         }
