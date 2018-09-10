fa3daf6d63d49c11f21b0d00598152edd208ab58
danmalmx <danmalmx@gmail.com>
2018-09-10 08:53:12 +0200

    changes to be able to do PR

--- src/js/fizz-buzz.js
+++ src/js/fizz-buzz.js
@@ -2,7 +2,7 @@ function FizzBuzz() {
     this.check = (number) => {
 
         if (number <= 0) {
-            return 'Positive integers only';   
+            return 'Positive int only';   
         } else if (number % 15 === 0) {
             return 'FizzBuzz';
         } else if (number % 5 === 0) {
